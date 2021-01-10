15abc8528fe8a630af11b4ee1c3360a26b663ca4
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-09 22:50:36 -0300

    command 'test'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config(); // sorito_bot
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -22,8 +23,25 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
+  const PREFIX = '!';
   const chatId = msg.chat.id;
 
+  if(msg.text.startsWith(PREFIX)){
+    // Get command name and arguments:
+    const [CMD_NAME, ...args] = msg.text
+    .toLowerCase()
+    .trim()
+    .substring(PREFIX.length)
+    .split(/\s+/);
+
+    // Commands filter:
+    if(CMD_NAME === 'test'){
+      bot.sendMessage(chatId, 'Test command');
+    }
+
+  }
+
+  
+
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
 });
